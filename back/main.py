d2d66995a054771506bb465b462cd06fde7e19ad
parker <236819579@qq.com>
2019-06-19 12:12:17 +0800

    1.BTC 1min

--- back/main.py
+++ back/main.py
@@ -5,6 +5,17 @@ from datetime import datetime
 import time
 
 
+# un_time = time.mktime(df.index[-1].timetuple())
+# un_time1 = time.mktime(df.index[-2].timetuple())
+# un_time2 = time.mktime(df.index[-3].timetuple())
+# un_time3 = time.mktime(df.index[-4].timetuple())
+# print(result[-4])
+# print(result[-3])
+# for i in range(len(df.index)):
+# print(un_time)
+# print(un_time1)
+# print(result[-1])
+
 # 用于调试
 class Main:
     # calc = Calc()
@@ -14,17 +25,6 @@ class Main:
     # result = klineDataTool.getFutureData('RB1910.XSGE', '240m', 200)
     # result = klineDataTool.getKlineData('3min',200)
 
-    df = get_price('RB1910.XSGE', frequency='1d', end_date=datetime.now(), count=200,
+    df = get_price('RB1910.XSGE', frequency='240m', end_date=datetime.now(), count=500,
                    fields=['open', 'high', 'low', 'close', 'volume'])
-
-    # un_time = time.mktime(df.index[-1].timetuple())
-    # un_time1 = time.mktime(df.index[-2].timetuple())
-    # un_time2 = time.mktime(df.index[-3].timetuple())
-    # un_time3 = time.mktime(df.index[-4].timetuple())
-    # print(result[-4])
-    # print(result[-3])
-    # for i in range(len(df.index)):
-    # print(un_time)
-    # print(un_time1)
-    # print(result[-1])
     print(df)
