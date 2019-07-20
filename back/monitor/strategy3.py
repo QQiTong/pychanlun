80570c2f6da4132de61663f6f43ff1cc23bfcb40
devdevdev <dev@shenqisoft.info>
2019-07-20 23:52:47 +0800

    STRATEGY3 XBTUSD 计算3m15m 背驰

--- back/monitor/strategy3.py
+++ back/monitor/strategy3.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 import time
 import pandas as pd
+import talib as ta
+import numpy as np
 from ..funcat.data.BitmexDataBackend import BitmexDataBackend
 from ..funcat.utils import get_int_date
 from ..KlineProcess import KlineProcess
 from ..BiProcess import BiProcess
 from ..DuanProcess import DuanProcess
+from .. import entanglement as entanglement
 
 def doMonitor1():
     """
@@ -38,6 +41,7 @@ def doMonitor1():
     #3m笔和段
     high3m = df3m.high.values
     low3m = df3m.low.values
+    close3m = df3m.close.values
     time3m = df3m.time.values
     count3m = len(time3m)
     klineProcess3m = KlineProcess()
@@ -51,9 +55,15 @@ def doMonitor1():
     duanProcess3m = DuanProcess()
     duanResult3m = duanProcess3m.handle(biResult3m, high3m, low3m)
 
+    diff3m, dea3m, macd3m = ta.MACD(np.array([float(x) for x in close3m]), fastperiod=12, slowperiod=26, signalperiod=9)
+    diff3m = np.nan_to_num(diff3m)
+    dea3m = np.nan_to_num(dea3m)
+    macd3m = np.nan_to_num(macd3m)
+
     #15m笔和段
     high15m = df15m.high.values
     low15m = df15m.low.values
+    close15m = df15m.close.values
     time15m = df15m.time.values
     count15m = len(time15m)
     klineProcess15m = KlineProcess()
@@ -66,3 +76,11 @@ def doMonitor1():
 
     duanProcess15m = DuanProcess()
     duanResult15m = duanProcess15m.handle(biResult15m, high15m, low15m)
+    diff15m, dea15m, macd15m = ta.MACD(np.array([float(x) for x in close15m]), fastperiod=12, slowperiod=26, signalperiod=9)
+    diff15m = np.nan_to_num(diff15m)
+    dea15m = np.nan_to_num(dea15m)
+    macd15m = np.nan_to_num(macd15m)
+
+    # 计算背驰
+    entanglementList3m = entanglement.calcEntanglements(time3m, duanResult3m, biResult3m, high3m, low3m)
+    entanglementList15m = entanglement.calcEntanglements(time15m, duanResult15m, biResult15m, high15m, low15m)
