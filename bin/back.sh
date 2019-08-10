505d043ea724c2e9c6456f85ad9a0977989539ff
devdevdev <dev@shenqisoft.info>
2019-08-11 00:18:20 +0800

    save

--- bin/back.sh
+++ bin/back.sh
@@ -5,8 +5,9 @@ name="pychanlun"
 docker rm -f $name
 docker rmi -f $name
 
-docker build -t $name .
+docker build -f Dockerfile.back -t $name .
 docker run --restart=always -d \
     --name $name \
+    --network pychanlun-net
     --expose 5000 \
     $name
---
+++ prepare.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python -m pip install --upgrade pip setuptools wheel
+
+python -m pip install ta-lib
