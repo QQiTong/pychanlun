ee536de03d4adc6537d4ce0317cfb9a96262c0c6
devdevdev <dev@shenqisoft.info>
2019-08-11 00:24:30 +0800

    save

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,12 @@
 
 python -m pip install --upgrade pip setuptools wheel
 
-python -m pip install ta-lib
+pushd /tmp
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+python -m pip install TA-Lib
+popd
