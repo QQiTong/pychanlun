e6bb54ad3abcbb5ec521e9d96ea31db88f820837
devdevdev <dev@shenqisoft.info>
2019-08-25 07:45:27 +0800

    save

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,12 @@
-workers = 5
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 bind = "0.0.0.0:5000"
+
+accesslog = "./log/access.log"
+errorlog = "./log/error.log"
+
+loglever = "info"
+
+debug = True
