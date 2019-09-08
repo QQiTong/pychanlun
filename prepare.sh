2ec5d804a212a388cabf921891ecfd24fe43c570
devdevdev <dev@shenqisoft.info>
2019-09-09 00:09:49 +0800

    save

--- prepare.sh
+++ prepare.sh
@@ -7,14 +7,11 @@ dpkg-reconfigure --frontend noninteractive tzdata
 
 python -m pip install --upgrade pip setuptools wheel
 
-pushd /tmp
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
 ./configure --prefix=/usr
 make -j
 make install
-popd
 
 python -m pip install numpy
 python -m pip install TA-Lib
