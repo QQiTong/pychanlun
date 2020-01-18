1741b8600a44a2a4c7bf4ecb9e5c642a563d2928
zerokldcty <1106628276@qq.com>
2020-01-18 21:52:36 +0800

    fix bi

--- back/basic/bi.py
+++ back/basic/bi.py
@@ -81,9 +81,9 @@ def IsBi(count, bi, high, low, from_index, to_index, dir, strict=False):
             isValid1 = False
             isValid2 = False
             for t in range(2, len(candlesCurr) - 2):
-                if not isValid1 and candlesCurr[t]['low'] < topLow:
+                if not isValid1 and candlesCurr[t]['high'] > bottomHigh:
                     isValid1 = True
-                if not isValid2 and candlesCurr[t]['high'] > bottomHigh:
+                if not isValid2 and candlesCurr[t]['low'] < topLow:
                     isValid2 = True
                 if isValid1 and isValid2:
                     break
