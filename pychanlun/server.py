d4aed2abb17d1ac0661bb8670b1fbb3ee44339dd
kldcty <1106628276@qq.com>
2020-04-23 17:00:17 +0800

    waitress

--- pychanlun/server.py
+++ pychanlun/server.py
@@ -6,7 +6,7 @@ import time
 
 import geventwebsocket
 from flask import Flask, request, Response
-from gevent.pywsgi import WSGIServer
+# from gevent.pywsgi import WSGIServer
 from waitress import serve
 
 import json
@@ -17,31 +17,31 @@ from pychanlun.config import config
 import rqdatac as rq
 from pychanlun.monitor.BusinessService import businessService
 from flask_sockets import Sockets
-from geventwebsocket.handler import WebSocketHandler
+# from geventwebsocket.handler import WebSocketHandler
 
 app = Flask(__name__)
 sockets = Sockets(app)
 # curl -X GET 'http://127.0.0.1:5000/api/stock_data?symbol=RB2005&period=5m'
 
 # socket 路由，访问url是： ws://localhost:5000/echo
-@sockets.route('/control')
-def echo_socket(ws):
-    while not ws.closed:
-        message = ws.receive()
-        clientEvent = json.loads(message)['event']
-        try:
-            if clientEvent == 'changeList':
-                while True:
-                    changeListResult = businessService.getChangeList()
-                    result = {
-                        'event': 'changeList',
-                        'data': changeListResult
-                    }
-                    # 每隔1秒更新价格信息
-                    ws.send(json.dumps(result))
-                    time.sleep(5)
-        except geventwebsocket.exceptions.WebSocketError as e:
-            pass
+# @sockets.route('/control')
+# def echo_socket(ws):
+#     while not ws.closed:
+#         message = ws.receive()
+#         clientEvent = json.loads(message)['event']
+#         try:
+#             if clientEvent == 'changeList':
+#                 while True:
+#                     changeListResult = businessService.getChangeList()
+#                     result = {
+#                         'event': 'changeList',
+#                         'data': changeListResult
+#                     }
+#                     # 每隔1秒更新价格信息
+#                     ws.send(json.dumps(result))
+#                     time.sleep(5)
+#         except geventwebsocket.exceptions.WebSocketError as e:
+#             pass
 # -------------------------------通用接口-----------------------------------
 @app.route('/api/stock_data')
 def data():
@@ -271,10 +271,10 @@ def run(**kwargs):
     # 程序启动初始化主力合约信息,不需要每次都请求
     businessService.initDoinantSynmbol()
     # 生产模式运行，用waitress服务器
-    # serve(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port)
     # waitress经常阻塞超时，换WSGIServer
-    http_serv = WSGIServer(("0.0.0.0", port), app, handler_class=WebSocketHandler)
-    http_serv.serve_forever()
+    # http_serv = WSGIServer(("0.0.0.0", port), app, handler_class=WebSocketHandler)
+    # http_serv.serve_forever()
 
 
 if __name__ == '__main__':
