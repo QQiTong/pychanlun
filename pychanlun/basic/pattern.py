0d4f1b0e743f51b0646452a8814789dbbd843fe1
kldcty <1106628276@qq.com>
2020-05-03 12:38:22 +0800

    save

--- pychanlun/basic/pattern.py
+++ pychanlun/basic/pattern.py
@@ -5,26 +5,26 @@ import pydash
 
 # 判断idx前面的形态是不是看多完备形态
 def perfect_buy_long(signal_series, high_series, low_series, idx):
-    d1 = FindPrevEq(signal_series, -1, idx + 1)
+    d1 = FindPrevEq(signal_series, -1, idx)
     g1 = FindPrevEq(signal_series, 1, d1)
     d2 = FindPrevEq(signal_series, -1, g1)
     g2 = FindPrevEq(signal_series, 1, d2)
     d3 = FindPrevEq(signal_series, -1, g2)
     if d3 >= 0 and low_series[d2] > low_series[d3] and low_series[d1] > low_series[d3]:
-        if low_series[d1] < low_series[idx] < high_series[g2]:
+        if low_series[d1] < low_series[idx] < max(high_series[g1], high_series[g2]):
             return True
     return False
 
 
 # 判断idx前面的形态是不是看跌完备形态
 def perfect_sell_short(signal_series, high_series, low_series, idx):
-    g1 = FindPrevEq(signal_series, 1, idx + 1)
+    g1 = FindPrevEq(signal_series, 1, idx)
     d1 = FindPrevEq(signal_series, -1, g1)
     g2 = FindPrevEq(signal_series, 1, d1)
     d2 = FindPrevEq(signal_series, -1, g2)
     g3 = FindPrevEq(signal_series, 1, d2)
     if g3 >= 0 and high_series[g2] < high_series[g3] and high_series[g1] < high_series[g3]:
-        if high_series[g1] > high_series[idx] > low_series[d2]:
+        if high_series[g1] > high_series[idx] > min(low_series[d2], low_series[d2]):
             return True
     return False
 
