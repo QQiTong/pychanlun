446ae1eb2224385ebd06051122203c9ad0027c19
kldcty <1106628276@qq.com>
2020-06-08 18:26:05 +0800

    save

--- pychanlun/monitor/stock_signal_calculator.py
+++ pychanlun/monitor/stock_signal_calculator.py
@@ -4,6 +4,7 @@ from pychanlun.db import DBPyChanlun
 import re
 import os
 import logging
+import decimal
 from datetime import datetime, timedelta, date
 from multiprocessing import Pool
 from bson.codec_options import CodecOptions
@@ -21,24 +22,21 @@ import numpy as np
 import pydash
 import pandas as pd
 
-
 tz = pytz.timezone('Asia/Shanghai')
 
 
 def run(**kwargs):
-    logger = logging.getLogger()
     # 清理掉1年以上的数据
     cutoff_time = datetime.now(tz) - timedelta(days=360)
-    DBPyChanlun["stock_signal"].with_options(codec_options=CodecOptions(
-        tz_aware=True, tzinfo=tz)).delete_many({
-            "fire_time": {"$lte": cutoff_time}
-        })
+    DBPyChanlun["stock_signal"] \
+        .with_options(codec_options=CodecOptions(tz_aware=True, tzinfo=tz)) \
+        .delete_many({"fire_time": {"$lte": cutoff_time}})
     code = kwargs.get('code')
     period = kwargs.get('period')
     codes = []
     if code is None:
-        collist = DBPyChanlun.list_collection_names()
-        for code in collist:
+        collection_list = DBPyChanlun.list_collection_names()
+        for code in collection_list:
             match = re.match("((sh|sz)(\\d{6}))_(30m|60m|240m)", code, re.I)
             if match is not None:
                 code = match.group(1)
@@ -47,43 +45,27 @@ def run(**kwargs):
     else:
         if period is None:
             for period in ['30m', '60m', '240m']:
-                codes.append({ 'code': code, 'period': period })
+                codes.append({'code': code, 'period': period})
         else:
-            codes.append({ 'code': code, 'period': period })
+            codes.append({'code': code, 'period': period})
 
     # 计算执缠策略
-    for idx in range(len(codes)):
-        info = codes[idx]
-        logger.info("%s/%s code=%s period=%s", idx+1, len(codes), info["code"], info["period"])
-        calculate(info)
-    # pool = Pool()
-    # pool.map(calculate, codes)
-    # pool.close()
-    # pool.join()
+    pool = Pool()
+    pool.map(calculate, codes)
+    pool.close()
+    pool.join()
 
-    # 计算连板
-    # pool = Pool()
-    # pool.map(raising_limit, codes)
-    # pool.close()
-    # pool.join()
-    codes = pydash.chain(codes).map(lambda x: x['code']).uniq().value()
-    for idx in range(len(codes)):
-        code = codes[idx]
-        logger.info("%s/%s code=%s", idx+1, len(codes), code)
-        raising_limit(code)
-
-    # 最近的100条记录输出到通达信软件
     export_to_tdx()
 
 
 def calculate(info):
-    logger = logging.getLogger()
     code = info["code"]
     period = info["period"]
 
     # 日线均线计算，只计算34日均线上的股票
-    bars = DBPyChanlun['%s_%s' % (code, '240m')].with_options(codec_options=CodecOptions(
-        tz_aware=True, tzinfo=tz)).find().sort('_id', pymongo.DESCENDING).limit(500)
+    bars = DBPyChanlun['%s_%s' % (code, '240m')] \
+        .with_options(codec_options=CodecOptions(tz_aware=True, tzinfo=tz)) \
+        .find().sort('_id', pymongo.DESCENDING).limit(500)
     bars = list(bars)
     bars.reverse()
     if len(bars) > 0:
@@ -92,9 +74,20 @@ def calculate(info):
         ma34 = ta.MA(np.array(close), timeperiod=34)
         if close[-1] < ma34[-1]:
             return
+        bars = pydash.chain(bars).takeRight(10).value()
+        c = 0
+        for x in range(1, len(bars)):
+            cur_close = decimal.Decimal('%.2f' % bars[x]['close'])
+            pre_close = decimal.Decimal('%.2f' % bars[x-1]['close'])
+            if cur_close >= (pre_close * decimal.Decimal('1.1')):
+                c = c + 1
+        if c == 0:
+            return
 
-    bars = DBPyChanlun['%s_%s' % (code, period)].with_options(codec_options=CodecOptions(
-        tz_aware=True, tzinfo=tz)).find().sort('_id', pymongo.DESCENDING).limit(1000)
+    bars = DBPyChanlun['%s_%s' % (code, period)] \
+        .with_options(codec_options=CodecOptions(tz_aware=True, tzinfo=tz))\
+        .find().sort('_id', pymongo.DESCENDING)\
+        .limit(1000)
     bars = list(bars)
     bars.reverse()
     if len(bars) == 0:
@@ -130,8 +123,8 @@ def calculate(info):
 
     DBPyChanlun['%s_%s' % (code, period)].with_options(codec_options=CodecOptions(
         tz_aware=True, tzinfo=tz)).delete_many({
-            "_id": {"$lt": cutoff_time}
-        })
+        "_id": {"$lt": cutoff_time}
+    })
 
     # 笔信号
     bi_series = [0 for i in range(count)]
@@ -151,8 +144,8 @@ def calculate(info):
                                  low_series, open_series, close_series, bi_series, duan_series)
     v_reverse = entanglement.v_reverse(entanglement_list, time_series, high_series,
                                        low_series, open_series, close_series, bi_series, duan_series)
-    duan_pohuai = entanglement.po_huai(time_series, high_series, low_series, open_series, close_series, bi_series, duan_series)
-
+    duan_pohuai = entanglement.po_huai(time_series, high_series, low_series, open_series, close_series, bi_series,
+                                       duan_series)
 
     higher_entaglement_list = entanglement.CalcEntanglements(
         time_series, higher_duan_series, duan_series, high_series, low_series)
@@ -217,7 +210,6 @@ def calculate(info):
         save_signal(code, period, '多-笔中枢三卖V', fire_time,
                     price, stop_lose_price, 'BUY_LONG', tags, category)
 
-
     count = len(duan_pohuai['buy_duan_break']['date'])
     for i in range(count):
         idx = duan_pohuai['buy_duan_break']['idx'][i]
@@ -230,15 +222,12 @@ def calculate(info):
 
 
 def save_signal(code, period, remark, fire_time, price, stop_lose_price, position, tags=[], category=""):
-    logger = logging.getLogger()
     # 股票只是BUY_LONG才记录
     if position == "BUY_LONG":
-        if (stop_lose_price - price) / price > -0.05:
-            logger.info("%s %s %s %s %s %s %s" %
-                        (code, period, remark, tags, category, fire_time, price))
-            DBPyChanlun['stock_signal'].with_options(codec_options=CodecOptions(tz_aware=True, tzinfo=tz)).find_one_and_update({
-                "code": code, "period": period, "fire_time": fire_time, "position": position
-            }, {
+        logging.info("%s %s %s %s %s %s %s" % (code, period, remark, tags, category, fire_time, price))
+        DBPyChanlun['stock_signal']\
+            .with_options(codec_options=CodecOptions(tz_aware=True, tzinfo=tz))\
+            .find_one_and_update({"code": code, "period": period, "fire_time": fire_time, "position": position}, {
                 '$set': {
                     'code': code,
                     'period': period,
@@ -254,36 +243,16 @@ def save_signal(code, period, remark, fire_time, price, stop_lose_price, positio
 
 
 def export_to_tdx():
-    logger = logging.getLogger()
     TDX_HOME = os.environ.get("TDX_HOME")
     if TDX_HOME is None:
-        logger.error("没有指定通达信安装目录环境遍历（TDX_HOME）")
+        logging.error("没有指定通达信安装目录环境遍历（TDX_HOME）")
         return
     seq = []
-    # 连扳票
-    raising_limit_stocks = DBPyChanlun['stock'].with_options(codec_options=CodecOptions(
-        tz_aware=True, tzinfo=tz)).find({"raising_limit_count": {"$gte": 2}}).sort('raising_limit_count', pymongo.DESCENDING)
-    c = 0
-    raising_limit_count = 0
-    for signal in list(raising_limit_stocks):
-        if signal["raising_limit_count"] != raising_limit_count:
-            raising_limit_count = signal["raising_limit_count"]
-            c = c + 1
-        if c <= 5:
-            code = signal["_id"]
-            if code.startswith("sh"):
-                code = code.replace("sh", "1")
-            elif code.startswith("sz"):
-                code = code.replace("sz", "0")
-            else:
-                continue
-            seq.append(code + "\n")
-        else:
-            break
-
-    # 缠论票
-    signals = DBPyChanlun['stock_signal'].with_options(codec_options=CodecOptions(
-        tz_aware=True, tzinfo=tz)).find({'period':{'$in':['30m','60m','240m']}}).sort('fire_time', pymongo.DESCENDING).limit(20)
+    signals = DBPyChanlun['stock_signal'] \
+        .with_options(codec_options=CodecOptions(tz_aware=True, tzinfo=tz)) \
+        .find({'period': {'$in': ['30m', '60m', '240m']}}) \
+        .sort('fire_time', pymongo.DESCENDING) \
+        .limit(30)
 
     for signal in list(signals):
         code = signal["code"]
@@ -297,60 +266,3 @@ def export_to_tdx():
     seq = pydash.uniq(seq)
     with open(os.path.join(TDX_HOME, "T0002\\blocknew\\CL%s.blk" % date.today().strftime("%d")), "w") as fo:
         fo.writelines(seq)
-
-
-def raising_limit(code):
-    """
-    广义的股票连扳数量，忽略最后2个涨停是一字板的股票，N天N-1或者N-2板有时候也算连板。
-    """
-    bars = DBPyChanlun['%s_240m' % code].with_options(codec_options=CodecOptions(
-        tz_aware=True, tzinfo=tz)).find().sort('_id', pymongo.DESCENDING).limit(30)
-    bars = list(bars)
-    if len(bars) < 3: return
-    count = 0
-    for idx in range(len(bars)-1):
-        if bars[idx]["close"] >= round(bars[idx+1]["close"]*1.1, 2):
-            count = count + 1
-        else:
-            break
-    if len(bars) >= 5:
-        xbars = bars[0:5]
-        if xbars[0]['close'] >= round(xbars[1]['close']*1.1, 2) and xbars[3]['close'] >= round(xbars[4]['close']*1.1, 2):
-            count = 4
-            for idx in range(4, len(bars)-1):
-                if bars[idx]["close"] >= round(bars[idx+1]["close"]*1.1, 2):
-                    count = count + 1
-                else:
-                    break
-        elif xbars[0]['close'] >= round(xbars[1]['close']*1.1, 2) and xbars[2]['close'] >= round(xbars[3]['close']*1.1, 2):
-            count = 3
-            for idx in range(3, len(bars)-1):
-                if bars[idx]["close"] >= round(bars[idx+1]["close"]*1.1, 2):
-                    count = count + 1
-                else:
-                    break
-    if len(bars) >= 10:
-        if bars[0]['close'] >= round(bars[1]['close']*1.1, 2):
-            x = -1
-            for x in range(4, 9):
-                if bars[x]['close'] >= round(bars[x+1]['close']*1.1, 2):
-                    if x > 0 and bars[0]['close'] > bars[x]['close']:
-                        count = x + 1
-                    break
-
-    if count > 1:
-        if  len(pydash.chain(bars).take(2).filter(lambda bar: bar["high"] != bar["low"]).value()) == 0:
-            # 最后2个涨停板是一字板的不要
-            count = 0
-        elif len(pydash.chain(bars[1:count]).filter(lambda bar: bar["high"] != bar["low"]).value()) == 0:
-            # 连续一字板首次开板的不要
-            count = 0
-
-    DBPyChanlun["stock"].with_options(codec_options=CodecOptions(
-        tz_aware=True, tzinfo=tz)).find_one_and_update({
-            "_id": code
-        }, {
-            "$set": {
-                "raising_limit_count": count
-            }
-        }, upsert=True)
