f0b8921348410f5df687ea4dcd0cd59c96f0ca45
zerodev <1106628276@qq.com>
2020-09-27 23:44:14 +0800

    save

--- pychanlun/analysis/chanlun_data.py
+++ pychanlun/analysis/chanlun_data.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from enum import Enum
+from typing import List
 
 
 # 方向枚举
-class Direction(Enum):
+class DirectionType(Enum):
+    # 向上方向
     UP = 1
+    # 向下方向
     DOWN = -1
+
+
+# 分型类型
+class FractalType(Enum):
+    # 顶分型
+    Top = 1
+    # 底分型
+    Bottom = -1
 
 
 # K线数据结构
@@ -37,11 +48,42 @@ class MergedStick:
     low_price: float
     high_high_price: float
     high_price: float
-    direction: Direction
+    direction: DirectionType
+    sticks: List[Stick]
 
-    def __int__(self, dt_start: int, dt_end: int, low_price: float, high_price: float, direction: Direction):
-        self.dt_start = dt_start
-        self.dt_end = dt_end
-        self.low_price = low_price
-        self.high_price = high_price
+    def __int__(self, stick: Stick, direction: DirectionType):
+        self.sticks = [stick]
+        self.dt_start = stick.dt
+        self.dt_end = stick.dt
+        self.low_low_price = stick.low_price
+        self.low_price = stick.low_price
+        self.high_high_price = stick.high_price
+        self.high_price = stick.high_price
         self.direction = direction
+
+    def add(self, stick: Stick):
+        self.dt_end = stick.dt
+        self.low_low_price = min(self.low_low_price, stick.low_price)
+        self.low_price = min(self.low_price, stick.low_price) if self.direction == DirectionType.DOWN else max(self.low_price, stick.low_price)
+        self.high_high_price = max(self.high_high_price, stick.high_price)
+        self.high_price = max(self.high_price, stick.high_price) if self.direction == DirectionType.UP else min(self.high_price, stick.high_price)
+
+
+# 分型
+class Fractal:
+    dt_start: int
+    dt_end: int
+    low_low_price: float
+    low_price: float
+    high_high_price: float
+    high_price: float
+    fractal_type: FractalType
+    merged_sticks = List[MergedStick]
+
+    def __init__(self, stick1: MergedStick, stick2: MergedStick, stick3: MergedStick, fractal_type: FractalType):
+        self.merged_sticks = [stick1, stick2, stick3]
+        self.low_low_price = min(stick1.low_low_price, stick2.low_low_price, stick3.low_low_price)
+        self.low_price = min(stick1.low_price, stick2.low_price, stick3.low_price)
+        self.high_high_price = max(stick1.high_high_price, stick2.high_high_price, stick3.high_high_price)
+        self.high_price = max(stick1.high_price, stick2.high_price, stick3.high_price)
+        self.fractal_type = fractal_type
