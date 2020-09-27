3bc12a4e007565fa5fba5a89577b3b739465b815
parker <236819579@qq.com>
2020-09-27 17:36:40 +0800

    1.fix

--- vnpy/vnpy/trader/utility.py
+++ vnpy/vnpy/trader/utility.py
@@ -15,7 +15,7 @@ import talib
 
 from .object import BarData, TickData
 from .constant import Exchange, Interval
-
+import time
 
 log_formatter = logging.Formatter('[%(asctime)s] %(message)s')
 
@@ -339,6 +339,12 @@ class ArrayManager(object):
         self.volume_array: np.ndarray = np.zeros(size)
         self.open_interest_array: np.ndarray = np.zeros(size)
 
+        # 我添加的
+        self.time_array = ['' for i in range(size)]
+        self.time_index_array = ['' for i in range(size)]
+
+
+
     def update_bar(self, bar: BarData) -> None:
         """
         Update new bar data into array manager.
@@ -354,12 +360,23 @@ class ArrayManager(object):
         self.volume_array[:-1] = self.volume_array[1:]
         self.open_interest_array[:-1] = self.open_interest_array[1:]
 
+        # 我添加的
+        self.time_array[:-1] = self.time_array[1:]
+        self.time_index_array[:-1] = self.time_index_array[1:]
+
+
         self.open_array[-1] = bar.open_price
         self.high_array[-1] = bar.high_price
         self.low_array[-1] = bar.low_price
         self.close_array[-1] = bar.close_price
         self.volume_array[-1] = bar.volume
         self.open_interest_array[-1] = bar.open_interest
+
+        #  我添加的
+        self.time_array[-1] = bar.datetime.strftime("%Y-%m-%d %H:%M")
+        date = time.strptime(self.time_array[-1], "%Y-%m-%d %H:%M")
+        self.time_index_array[-1] = time.mktime(date)
+
 
     @property
     def open(self) -> np.ndarray:
