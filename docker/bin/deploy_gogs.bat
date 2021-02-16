8f4407caa22cf45c5951e40d0c11de2080791046
1106628276 <1106628276@qq.com>
2021-02-16 23:17:17 +0800

    save

--- docker/bin/deploy_gogs.bat
+++ docker/bin/deploy_gogs.bat
@@ -1,2 +1,2 @@
 docker rm -f gogs
-docker run -d --name=gogs --restart=always --network qanetwork -v /opt/gogs:/data gogs/gogs
+docker run -d --name=gogs --restart=always --network qanetwork -p 3000:3000 -v gogs_data:/data gogs/gogs
